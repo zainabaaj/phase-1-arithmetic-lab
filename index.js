3f71d88ba73731db88de3b5ae5562665e5395620
Zeyna Baaj <zainabaaj@gmail.com>
2023-04-23 07:56:45 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let multiply
+let num1 = 2
+let num2 = 31
+multiply = num1 * num2
+
+let random
+random = Math.floor(Math.random() * 10) + 1;
+let mod
+let num3 = 16
+let num4 = 6
+mod = num3 % num4
+let max = Math.max(1, 15, 20, 7, 5);
